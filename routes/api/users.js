fcb3eb9eb51adaa2c472fdaace4a3096a4c0cb91
Linda Wang <lindawang256@gmail.com>
2018-06-09 08:19:43 -0700

    add passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+//const User = mongoose.model('users');
+// load User model
+
+//load Input Validation
+const validateRegisterInput = require ('../../validation/register');
+
+const User = require('../../models/User') // can use mongoose methods after loading User model
+
+
+
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -8,5 +23,111 @@ const router = express.Router();
 // becouse we use rounter, we don't need to write './routes/api/users/test'
 router.get('/test',(req,res) => res.json({msg: "Users works"})); //res.json will automatically send code 200. res.json is same as res.send but send output json.
 
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req,res) =>{
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //check validation
+  if (!isValid){
+    return res.status(400).json(errors);
+  }
+
+  const userEmail = req.body.email // when we send data to a route through a post requests which will utilmately be trhough a form in our react application. We access the data by req.body. Need import bodyParser in server.js
+  // findOne() mongoose method
+  User.findOne({email:userEmail}) // mongoose can use callback function of promise(.then)
+    .then(user => {
+      if (user){
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors.email);
+      } else {
+        const avatar = gravatar.url(req.body.email,{
+          s:'200', // Size
+          r: 'pg', //Rating
+          d:'mm' //Default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar, // same as avatar: avatar
+          password:req.body.password
+        });
+
+        bcrypt.genSalt(10,(err,salt) => {
+          bcrypt.hash(newUser.password,salt,(err,hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
+
+
+
+// @route   GET api/users/login
+// @desc    Tests users route
+// @access  Public
+
+router.post('/login', (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({email})
+  .then(user => {
+    if (!user) {
+      return res.status(404).json({email: 'User not found'});
+    }
+
+    bcrypt.compare(password,user.password)
+      .then(isMatch => {
+        if(isMatch){
+          //User Matched
+
+          const payload ={id:user.id, name: user.name, avatar: user.avatar} // Create jwt payload
+
+          //Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 }, //3600 is an hour
+            (err,token) => {
+              res.json({
+                success: true,
+                token: 'Bearer '+ token
+              });
+          });
+        } else {
+          return res.status(400).json({password : 'Password incorrect'});
+        }
+      })
+  })
+
+})
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+
+// router.get('/current', passport.authenticate('jwt', {session: false}), (req,res) => {res.json({msg:'success'})
+//   }
+//   )
+router.get('/current', passport.authenticate('jwt', { session: false}), (req,res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+)
+
 module.exports = router; // export router for server.js to pick it up
+
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -7,8 +9,12 @@ const profile = require('./routes/api/profile');
 
 const app=express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false})); //basically tells the system whether you want to use a simple algorithm for shallow parsing (i.e. false) or complex algorithm for deep parsing that can deal with nested objects (i.e. true).
+app.use(bodyParser.json());
+
 //DB config
-const db = require('./config/key').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 //Connect to MongoDB
 mongoose
@@ -17,6 +23,12 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req,res)=> res.send('Good morning Linda!'));
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport Config
+require('./config/passport')(passport);
 
 //use routes
 app.use('/api/users',users); // send '/api/users' to users.js, then on user.js file don't need to write '/api/users';
