6cd32659f5ad53b5c9e0f3245f9efa3bb64fca7f
Linda Wang <lindawang256@gmail.com>
2018-06-23 07:55:32 -0700

    before apply redux

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware} from 'redux';
+
+import Navbar from './components/layout/Navbar.js'
+import Footer from './components/layout/Footer.js'
+import Landing from './components/layout/Landing.js'
+import Register from './components/auth/Register.js'
+import Login from './components/auth/Login.js'
+
+
+import './App.css';
+
+const store =createStore( () =>[],{},applyMiddleware());
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store = {store}>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <Route exact path="/" component= {Landing} />
+              <div className = 'container'>
+                  <Route exact path="/register" component= {Register} />
+                  <Route exact path="/login" component= {Login} />
+              </div>
+              <Footer />
+            </div>
+          </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
